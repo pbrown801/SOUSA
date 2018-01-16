e19f7f4a19d5fee01aacfcb2678070adefc5ec3d
Tate Walker <tatewalker@tamu.edu>
2018-01-15 21:53:40 -0600

    updated randomGal to get Av at various distances from random coordinates

--- randomGal.py
+++ randomGal.py
@@ -21,13 +21,6 @@ from random import randint
 
 #LOOK AT QUICKDATA.PY FOR MULTIPLE GALAXY CONFIG
 
-# galaxy = input("Enter galaxy followed by [Enter]\n")
-# try:
-# 	tempCoord = SkyCoord.from_name(galaxy, frame = 'icrs')
-# except NameResolveError:
-# 	print("Can't retrieve coordinates for galaxy, please check the name and try again.")
-
-# >>> c = SkyCoord('00 42 30 +41 12 00', unit=(u.hourangle, u.deg))
 def getRandomCoordinate():
 	h = randint(0,23)
 	# print("h = " + str(h))
@@ -49,44 +42,116 @@ def getRandomCoordinate():
 	# print(dec.to_string())
 	return SkyCoord(ra, dec, frame = 'fk5')
 
+def timeFix(s,m,h): #fixes time to ensure it stays within normal range (0-60)
+	if(s>=60 or m>=60):
+		while(s>=60 or m>=60):
+			if s >= 60:
+				m+=1
+				s-=60
+			if m >= 60:
+				h+=1
+				m-=60
+	elif(s<0 or m<0):
+		while(s<0 or m<0):
+			if s < 0:
+				m-=1
+				s+=60
+			if m < 0:
+				h-=1
+				m+=60		
+	return s,m,h;
+
+
+def fourCoord(dam,ra,dec,coord):
+
+	ds = dam*4
+
+	#e
+	ds/=math.cos(math.radians(dec.degree))
+	h = ra.hms.h
+	m = ra.hms.m
+	s = ra.hms.s+ds
+	(s,m,h) = timeFix(s,m,h)
+	rad = Angle((h,m,s), unit = u.hour)
+	rad = Angle(rad.to_string(unit=u.hour),u.hour)
+	coord[1] = rad.to_string()+" "+dec.to_string()
+
+	#n
+	decli = dec.arcminute+dam
+	decl = Angle(decli,u.arcminute)
+	decl = Angle(decl.to_string(unit=u.degree),u.degree)
+	coord[0] = ra.to_string()+" "+decl.to_string()
+	
+	#w
+	ds=ds*(-1)
+	ds/=math.cos(math.radians(dec.degree))
+	h = ra.hms.h
+	m = ra.hms.m
+	s = ra.hms.s+ds
+	(s,m,h) = timeFix(s,m,h)
+	rad = Angle((h,m,s), unit = u.hour)
+	rad = Angle(rad.to_string(unit=u.hour),u.hour)
+	coord[3] = rad.to_string()+" "+dec.to_string()
+
+	#s
+	decli = dec.arcminute-dam
+	decl = Angle(decli,u.arcminute)
+	decl = Angle(decl.to_string(unit=u.degree),u.degree)
+	coord[2] = ra.to_string()+" "+decl.to_string()
+	
+	#print(coord)
+	return coord; #performs transformation of initial coord into cardinal coordinates 
+
+def tableFill(dam, ra, dec):
+	# curVal = [None] *4 #n = 0, e = 1, s = 2, w = 3
+	coord = [None] *4 #n = 0, e = 1, s = 2, w = 3
+	#get values for each arcminute
+	for j in range(0,dam+1):
+		fourCoord(j, ra, dec, coord)
+		with open('randomAv.csv','a') as output:
+			output.write(str(j)+',')
+			for i in range(0,4):
+
+				C = coordinates.SkyCoord(coord[i])
+				table = IrsaDust.get_extinction_table(C.fk5, show_progress = False)
+				output.write(str(table['A_SandF'][2])+',')
+				# curVal[i] = (table['A_SandF'][2])	
+			output.write('\n')
+
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rloading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print(chr(27) + "[2J")
+    sys.stdout.write('\rDone!')
+
 
 coords = []
-av = []
-
-write_file = 'randomAV.csv'
-with open(write_file,'w') as output:
-	output.write("RA, DEC, AV \n")
 
 for i in range(0,10): #get x number of random coordinates
 	coords.append(getRandomCoordinate())
-	print(coords[i])
-	# print(coords[i].ra.to_string(unit = u.hour))
-	table = IrsaDust.get_extinction_table(coords[i], show_progress = False)
-	av.append(table['A_SandF'][2])
-	print(av[i])
+	# print(coords[i])
 
-	with open(write_file, 'a') as output:
-		output.write(str(coords[i].ra.to_string(unit = u.hour)) + ',' + str(coords[i].dec.to_string()) + ',' + str(av[i]) + '\n')
+write_file = 'randomAV.csv'
+arcMinutes = 5
 
 
-	
+# threading
+done = False
+print(chr(27) + "[2J")
+threader = threading.Thread(target=animate)
+threader.start()
 
+with open(write_file,'w'):
 
+	for i in range(0,len(coords)):
 
+		with open(write_file,'a') as output:
+			output.write(str(coords[i].ra.to_string(unit = u.hour)) + ',' + str(coords[i].dec.to_string()) + ", \n Arcminute, North, East, South, West,\n")
+		tableFill(arcMinutes,coords[i].ra,coords[i].dec)
+		print('\n'+str(len(coords)-i) + " left!")
 
-
-# done = False
-# print(chr(27) + "[2J")
-# threader = threading.Thread(target=animate)
-# threader.start()
-
-# write_file = 'randVals.csv'
-# with open(write_file,'w') as output:
-# 	output.write("\n")
-# 	ra = Angle(tempCoord.ra.hour,unit = u.hour)
-# 	dec = tempCoord.dec
-	
-# 	with open(write_file, 'a') as output:
-# 		output.write( '\n')
-# 		#print(gals[i] + " Done!")
-# done = True
+done = True
