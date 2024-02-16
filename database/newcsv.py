abee66707936eefa7370bf961349f9bbbfb10178
tmasha <thomas8483828180@gmail.com>
2024-02-15 20:29:46 -0600

    Database now has redshifts.

--- database/newcsv.py
+++ database/newcsv.py
@@ -6,12 +6,12 @@ f = open('found.json')
 found = json.load(f)
 f.close()
 
-names, types = [], []
+names, types, redshifts = [], [], []
 for i, j in found.items():
     names.append(i)
     types.append(j)
+    redshifts.append('')
     print(f'Added {i} with type {j}.')
-
 
 # append the updated.csv names, types
 with open('updated.csv', newline='') as csvfile:
@@ -21,7 +21,8 @@ with open('updated.csv', newline='') as csvfile:
         if '_dup' in row[0]: continue
         names.append(row[0])
         types.append(row[1])
-        print(f'Added {row[0]} with type {row[1]}.')
+        redshifts.append(row[2])
+        print(f'Added {row[0]} with type {row[1]} and redshift {row[2]}.')
         
 # get PTFs
 with open('PTF_CClist.txt', 'r') as ptflist:
@@ -29,7 +30,8 @@ with open('PTF_CClist.txt', 'r') as ptflist:
         line = line.split()        
         names.append(line[0])
         types.append(line[1])
-        print(f'Added {line[0]} with type {line[1]}.')
+        redshifts.append(line[2])
+        print(f'Added {line[0]} with type {line[1]} and redshift {line[2]}.')
 
 # load all the json data as lowercased
 with open('altnames.json', 'r') as json_file:
@@ -44,9 +46,9 @@ for key, value in json_data.items():
         curr.append(item.lower())
     altnames[key] = curr
 
-# search for cross-matches
+# search for cross-matches and missing redshifts
 changed = 0
-prefixes = ['ztf', 'ptf', 'iptf', 'atlas', 'dlt', 'gaia', 'asassn', 'pgir']
+prefixes = ['ztf', 'ptf', 'iptf', 'atlas', 'dlt', 'gaia', 'asassn']
 for i, name in enumerate(names):
     name = name.lower()
     # skip any nova that does not have these prefixes
@@ -61,14 +63,33 @@ for i, name in enumerate(names):
             break
 
 # reformat SN supernovae so they have the correct(?) casing
-print('Formatting...')
+print(f'{changed} cross-matches found. Formatting...')
 for i, name in enumerate(names):
     if name[:2].lower() == 'sn':
         names[i] = 'SN' + name[2:].lower()
-        print(f'{name} was updated to {names[i]}')
 
+# search for missing redshifts
+print('Searching for missing redshifts...')
+names_shifts = {}
+with open('tns.csv', 'r') as tnscsv:
+    reader = csv.reader(tnscsv)
+    next(reader, None)
+    next(reader, None)
+    for row in reader:
+        print(f'SN{row[0]} -> {row[5]}?')
+        names_shifts['SN' + row[2]] = row[5]
+
+for i, name in enumerate(names):
+    if redshifts[i] == '':
+        try:
+            redshifts[i] = names_shifts[name]
+            print(f"{name}'s redshift was updated to {names_shifts[name]}.")
+        except:
+            print(f'No redshift found for {name}.')
+
+# create df, write to csv
 print('Creating dataframe...')
-df = pd.DataFrame(list(zip(names, types)), columns = ['name', 'type'])
+df = pd.DataFrame(list(zip(names, types, redshifts)), columns = ['name', 'type', 'redshift'])
 
 print ('Dropping duplicates...')
 df.drop_duplicates()
